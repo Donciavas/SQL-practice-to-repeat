661ba6a405811e35c61a732a31c4a6617f010a65
Donatas Vitkus <vitk.donatas@gmail.com>
2023-02-10 20:17:07 +0200

    Repeated SQL basics, queries, CRUD operations .

---
+++ MS_SQL_Projektu_valdymas_Queries/Projektu_Valdymas_Db_creation.sql
@@ -0,0 +1,110 @@
+﻿USE Projektu_valdymas;
+CREATE TABLE PROJEKTAS(
+    "ID" INTEGER NOT NULL,
+    "PAVADINIMAS" VARCHAR(255)
+);   
+            
+INSERT INTO PROJEKTAS VALUES
+(1, 'Izola'),
+(2, 'Registrų Centras'),
+(3, 'Kaunas');   
+        
+CREATE  TABLE SKYRIUS(
+    PAVADINIMAS VARCHAR(255) NOT NULL,
+    VADOVAS_ASMENSKODAS VARCHAR(255)
+); 
+
+INSERT INTO SKYRIUS VALUES
+('Java', '48509141175'),
+('Testavimo', '38804172782'),
+('C#', '38904172782');        
+
+CREATE TABLE "DARBUOTOJAS"(
+    ASMENSKODAS CHAR(11) NOT NULL,
+    VARDAS VARCHAR(255),
+    PAVARDE VARCHAR(255),
+    DIRBANUO DATE,
+    GIMIMOMETAI DATE,
+    PAREIGOS VARCHAR(255),
+    SKYRIUS_PAVADINIMAS VARCHAR(255),
+    PROJEKTAS_ID INTEGER
+);              
+         
+INSERT INTO [DARBUOTOJAS] VALUES
+('38101122335', 'Petras', 'Petraitis', '2009-10-30', '1981-01-11', 'Testuotojas', 'Testavimo', 1),
+('38010101234', 'Jonas', 'Jonaitis', '2007-05-30', '1980-10-10', 'Programuotojas', 'Java', 2),
+('38103201435', 'Rimas', 'Plekaitis',  '2009-10-30',  '1981-03-20', 'Programuotojas', 'Java', 3),
+('48509141175', 'Zita', 'Lietuvaitė',  '2012-06-15',  '1985-09-14', 'Projektų vadovė', 'Java', 2),
+('48410121275', 'Jurga', 'Jurgaityte',  '2011-02-12',  '1984-10-12', 'Programuotoja', 'C#', 1),
+('38807201234', 'Giedrius', 'Sabutis',  '2009-01-21',  '1988-07-20', 'Testuotojas', 'Testavimo', 2),
+('38807291234', 'Regimantas', 'Sabonis',  '2013-01-21',  '1988-07-29', 'Testuotojas', 'Testavimo', 3),
+('38609191234', 'Saulius', 'Sabonis',  '2013-01-21',  '1986-09-19', 'Testuotojas', 'Testavimo', 3),
+('38109197598', 'Justas', 'Sabonis',  '2011-12-17',  '1986-09-19', 'Testuotojas', 'Testavimo', 1),
+('38503142412', 'Jonas', 'Kalnas',  '2009-05-11',  '1985-03-24', 'Programuotojas', 'Java', 1),
+('39003142412', 'Stasys', 'Sakalas',  '2009-05-11',  '1990-03-24', 'Programuotojas', 'Java', 3),
+('37803142412', 'Povilas', 'Vakalas',  '2012-11-11',  '1978-03-14', 'Programuotojas', 'C#', 2),
+('38804172782', 'Deivydas', 'Piliukas',  '2011-12-11',  '1988-04-17', 'Projektu vadovas', 'Testavimo', 2),
+('38904172782', 'Kestas', 'Liutas',  '2012-12-11',  '1989-04-17', 'Projektu vadovas', 'C#', 1),
+('38901228523', 'laikinas', 'veikejas',  '2009-01-22',  '1989-01-22', NULL, NULL, NULL);
+
+CREATE TABLE Projektai (
+    "Nr" integer NOT NULL,
+    "Pavadinimas" VARCHAR(255),
+    "Svarba" VARCHAR(255),
+    "Pradžia" date,
+    "Trukmė" integer
+);
+
+CREATE TABLE Vykdymas (
+    "Projektas" integer NOT NULL,
+    "Vykdytojas" integer NOT NULL,
+    "Statusas" VARCHAR(255),
+    "Valandos" integer
+);
+
+CREATE TABLE Vykdytojai (
+    "Nr" integer NOT NULL,
+    "Pavardė" VARCHAR(255),
+    "Kvalifikacija" VARCHAR(255),
+    "Kategorija" integer,
+    "Išsilavinimas" VARCHAR(255)
+);
+
+INSERT INTO Projektai ("Nr", "Pavadinimas", "Svarba", "Pradžia", "Trukmė") VALUES
+(1,	'Studentų apskaita',	'Aukšta',	'2015-01-01',	12),
+(2,	'Buhalterinė apskaita',	'Vidutinė',	'2005-03-01',	10),
+(3,	'WWW svetainė',			'Ypatinga',	'2005-06-01',	2
+);
+
+
+INSERT INTO Vykdymas ("Projektas", "Vykdytojas", "Statusas", "Valandos") VALUES
+(1,	1,	'Programuotojas', 30),
+(1,	2,	'Dokumentuotojas', 100),
+(1,	3,	'Testuotojas', 100),
+(1,	4,	'Vadovas', 100),
+(2,	1,	'Programuotojas', 300),
+(2,	2,	'Analitikas',	250),
+(2,	34,	'Vadovas',	100),
+(3,	1,	'Programuotojas', 250),
+(3,	2,	'Vadovas',	400),
+(3,	3,	'Dizaineris',	150
+);
+
+INSERT INTO Vykdytojai ("Nr", "Pavardė", "Kvalifikacija", "Kategorija", "Išsilavinimas") VALUES
+(1,	'Jonaitis',	'Informatikas'	,2,	'VU'),
+(2,	'Petraitis','Statistikas'	,3,	'VU'),
+(3,	'Gražulytė','Inžinierius'	,1,	'\N'),
+(4,	'Onaitytė',	'Vadybininkas'	,5,	'VDU'),
+(5,	'Antanaitis','Informatikas'	,3,	'VU'
+);
+
+ALTER TABLE "Projektai"
+    ADD CONSTRAINT "Projektai_pkey" PRIMARY KEY ("Nr");
+
+ALTER TABLE "Vykdymas"
+ADD CONSTRAINT "Vykdymas_pkey" PRIMARY KEY ("Projektas", "Vykdytojas");
+
+ALTER TABLE "Vykdytojai"
+    ADD CONSTRAINT "Vykdytojai_pkey" PRIMARY KEY ("Nr");
+
+
---
+++ MS_SQL_Projektu_valdymas_Queries/Projektu_valdymas_Aggregate_Queries.sql
@@ -0,0 +1,169 @@
+﻿USE Projektu_valdymas;
+
+--SELECT coulmn, group_function
+--FROM table
+--[WHERE condition]
+--[GROUP BY list_to_group]
+--[HAVING group_condition]
+--[ORDER BY sorting_condition]
+
+SELECT [ASMENSKODAS]
+      ,[VARDAS]
+      ,[PAVARDE]
+      ,[DIRBANUO]
+      ,[GIMIMOMETAI]
+      ,[PAREIGOS]
+      ,[SKYRIUS_PAVADINIMAS]
+      ,[PROJEKTAS_ID]
+  FROM [Projektu_valdymas].[dbo].[DARBUOTOJAS];
+
+  SELECT [ASMENSKODAS],[VARDAS],[PAVARDE] FROM [dbo].[DARBUOTOJAS] WHERE [GIMIMOMETAI] = '1988-07-20';
+
+  SELECT [DIRBANUO],[GIMIMOMETAI] FROM [dbo].[DARBUOTOJAS] 
+  WHERE [GIMIMOMETAI] <= '1988-07-29';
+
+  SELECT * FROM [dbo].[DARBUOTOJAS] 
+  WHERE [DIRBANUO] <= '2012-11-11' AND [DIRBANUO] >= '2009-10-30';
+
+  SELECT [VARDAS],[PROJEKTAS_ID],[ASMENSKODAS] FROM [dbo].[DARBUOTOJAS] 
+  WHERE [PROJEKTAS_ID] IN (2, 3);
+
+  SELECT VARDAS, PAVARDE, ASMENSKODAS FROM DARBUOTOJAS WHERE PAVARDE LIKE '%e';
+
+  SELECT * FROM DARBUOTOJAS WHERE GIMIMOMETAI LIKE '%12';
+
+  SELECT PAREIGOS FROM DARBUOTOJAS WHERE PAREIGOS LIKE '____u%';
+
+  SELECT * FROM DARBUOTOJAS WHERE SKYRIUS_PAVADINIMAS is NULL;
+
+  SELECT [DIRBANUO],[GIMIMOMETAI] FROM [DARBUOTOJAS] 
+  WHERE [GIMIMOMETAI] != '1988-07-29';
+
+  SELECT [VARDAS] FROM [DARBUOTOJAS] WHERE [VARDAS] NOT LIKE 'S%';
+
+  SELECT [VARDAS],[DIRBANUO],[GIMIMOMETAI] FROM [DARBUOTOJAS] WHERE NOT [DIRBANUO] >= '2009-10-30' AND [DIRBANUO] <= '2012-11-11';
+  
+  SELECT MAX([PROJEKTAS_ID]), MIN([PROJEKTAS_ID]) FROM [DARBUOTOJAS];
+
+  SELECT [PROJEKTAS_ID], COUNT([PROJEKTAS_ID]) FROM [Projektu_valdymas].[dbo].[DARBUOTOJAS] GROUP BY [PROJEKTAS_ID] HAVING COUNT([PROJEKTAS_ID]) > 1;
+
+SELECT [PROJEKTAS_ID], COUNT([PROJEKTAS_ID])
+FROM [Projektu_valdymas].[dbo].[DARBUOTOJAS]
+GROUP BY [PROJEKTAS_ID]
+HAVING COUNT([PROJEKTAS_ID]) > 1;
+
+SELECT [PAREIGOS],[PROJEKTAS_ID],COUNT([PROJEKTAS_ID])
+FROM [Projektu_valdymas].[dbo].[DARBUOTOJAS]
+WHERE [PAREIGOS] LIKE 'Program%'
+GROUP BY [PROJEKTAS_ID],[PAREIGOS]
+HAVING COUNT([PROJEKTAS_ID]) >= 1;
+
+SELECT [VARDAS],[PAVARDE],[PROJEKTAS_ID],COUNT([PROJEKTAS_ID])
+FROM [Projektu_valdymas].[dbo].[DARBUOTOJAS]
+GROUP BY [PROJEKTAS_ID],[VARDAS],[PAVARDE];
+
+SELECT "Pavardė" FROM [Projektu_valdymas].[dbo]."Vykdytojai", [Projektu_valdymas].[dbo]."Vykdymas" WHERE "Vykdytojas" = "Nr" AND "Projektas" = 1;
+
+SELECT [Pavardė], [Pavadinimas], [Valandos]
+FROM [Projektu_valdymas].[dbo].[Vykdytojai], [Projektu_valdymas].[dbo].[Projektai], [Projektu_valdymas].[dbo].[Vykdymas]
+WHERE [Projektas] = [Projektai].[Nr] AND [Vykdytojas] = [Vykdytojai].[Nr];
+
+SELECT "A"."Pavardė", "B"."Pavardė" FROM [Projektu_valdymas].[dbo]."Vykdytojai" "A", [Projektu_valdymas].[dbo]."Vykdytojai" "B" WHERE "A"."Kvalifikacija" = "B"."Kvalifikacija" AND "A"."Nr" < "B"."Nr";
+
+SELECT DISTINCT "A"."Pavardė", "B"."Pavardė" FROM [Projektu_valdymas].[dbo]."Vykdytojai" "A", [Projektu_valdymas].[dbo]."Vykdytojai" "B" WHERE "A"."Kvalifikacija" = "B"."Kvalifikacija";
+
+SELECT "A"."Pavardė", "B"."Pavardė"
+FROM [Projektu_valdymas].[dbo]."Vykdytojai" "A", [Projektu_valdymas].[dbo]."Vykdytojai" "B"
+WHERE "A"."Kvalifikacija" = "B"."Kvalifikacija"
+AND "A"."Nr" <> "B"."Nr";
+
+SELECT "Pavardė", "Pavadinimas", "Valandos" 
+FROM [Projektu_valdymas].[dbo]."Vykdytojai", [Projektu_valdymas].[dbo]."Projektai", [Projektu_valdymas].[dbo]."Vykdymas" 
+WHERE "Projektas" = "Projektai"."Nr" 
+AND "Vykdytojas" = "Vykdytojai"."Nr" 
+AND "Svarba" = 'Ypatinga';
+
+--Projekto Nr. 1 vykdytojų pavardės:
+SELECT "Pavardė" FROM [Projektu_valdymas].[dbo]."Vykdytojai" JOIN [Projektu_valdymas].[dbo]."Vykdymas" ON "Vykdytojas" = "Nr" WHERE "Projektas" = 1;
+
+--Užklausa be JOIN:
+SELECT "Pavardė" FROM [Projektu_valdymas].[dbo]."Vykdytojai", [Projektu_valdymas].[dbo]."Vykdymas" WHERE "Vykdytojas" = "Nr" AND "Projektas" = 1;
+
+--Galima jungti ir daugiau lentelių, pavyzdžiui:
+
+SELECT "Pavardė", "Pavadinimas", "Valandos" FROM([Projektu_valdymas].[dbo]."Vykdytojai" JOIN [Projektu_valdymas].[dbo]."Vykdymas" ON "Vykdytojas" = "Vykdytojai"."Nr")JOIN [Projektu_valdymas].[dbo]."Projektai" ON "Projektas" = "Projektai"."Nr";
+
+--Užklausa be JOIN:
+SELECT "Pavardė", "Pavadinimas", "Valandos" FROM [Projektu_valdymas].[dbo]."Vykdytojai", [Projektu_valdymas].[dbo]."Projektai", [Projektu_valdymas].[dbo]."Vykdymas" WHERE "Projektas" = "Projektai"."Nr" AND "Vykdytojas" = "Vykdytojai"."Nr";
+
+--Visų projekto Nr. 1 vykdytojų pavardės, statusai ir valandos:
+
+SELECT "Pavardė", "Statusas", "Valandos"FROM [Projektu_valdymas].[dbo]."Vykdytojai" JOIN [Projektu_valdymas].[dbo]."Vykdymas" ON "Vykdytojas" = "Nr" WHERE "Projektas" = 1;
+
+--Darbuotojai ir jų dalyvavimas projekte Nr. 1:
+SELECT "Pavardė", "Statusas", "Valandos"FROM [Projektu_valdymas].[dbo]."Vykdytojai" LEFT OUTER JOIN [Projektu_valdymas].[dbo]."Vykdymas" ON "Nr" = "Vykdytojas" WHERE "Projektas" = 1;
+
+--Pavardės vykdytojų, dalyvaujančių projekte Nr. 1:
+
+SELECT "Pavardė" FROM [Projektu_valdymas].[dbo]."Vykdytojai" WHERE "Nr" IN(SELECT "Vykdytojas" FROM [Projektu_valdymas].[dbo]."Vykdymas" WHERE "Projektas" = 1);
+
+--Vykdytojai, dalyvaujantys bent viename ypatingos svarbos projekte:
+
+SELECT "Pavardė" FROM [Projektu_valdymas].[dbo]."Vykdytojai" WHERE "Nr" IN
+(SELECT "Vykdytojas" FROM [Projektu_valdymas].[dbo]."Vykdymas" WHERE "Projektas" IN
+(SELECT "Nr" FROM [Projektu_valdymas].[dbo]."Projektai" WHERE "Svarba" = 'Ypatinga'));
+
+SELECT "Vykdytojas","Pavardė", "Pavadinimas" FROM([Projektu_valdymas].[dbo]."Vykdytojai" JOIN [Projektu_valdymas].[dbo]."Vykdymas" ON "Vykdytojas" = "Vykdytojai"."Nr")JOIN [Projektu_valdymas].[dbo]."Projektai" ON "Projektas" = "Projektai"."Nr";
+
+SELECT "Vykdytojas","Pavardė", "Pavadinimas" FROM([Projektu_valdymas].[dbo]."Vykdytojai" JOIN [Projektu_valdymas].[dbo]."Vykdymas" ON "Vykdytojas" = "Vykdytojai"."Nr")JOIN [Projektu_valdymas].[dbo]."Projektai" ON "Projektas" = "Projektai"."Nr" WHERE "Svarba" = 'Ypatinga';
+
+SELECT "Vykdytojas","Pavardė","Statusas" FROM([Projektu_valdymas].[dbo]."Vykdytojai" JOIN [Projektu_valdymas].[dbo]."Vykdymas" ON "Vykdytojas" = "Vykdytojai"."Nr") WHERE "Statusas" = 'Testuotojas';
+
+--Išrinkite visas moteris dirbančias projekte Izola ir išveskite į ekraną jų vardus, pavardes ir projekto pavadinimą.
+SELECT DISTINCT "Vykdytojas","Pavardė", "Pavadinimas" FROM([Projektu_valdymas].[dbo]."Vykdytojai" JOIN [Projektu_valdymas].[dbo]."Vykdymas" ON "Vykdytojas" = "Vykdytojai"."Nr")JOIN [Projektu_valdymas].[dbo]."Projektai" ON "Projektas" = "Projektai"."Nr" WHERE "Pavardė" LIKE '%e';
+
+--Išrinkite skyrių pavadinimus su juose dirbančių darbuotojų skaičiumi.
+
+--Select Top N per Category
+--Task: Extract the names and classes for the top 2 test scores for each class in the school, ordered by class and then by score. If there are any tie breakers, use the lower student_id to break the tie.
+
+--WITH class_ranking AS (
+--SELECT
+--    c.class, n.name, s.score,
+--    ROW_NUMBER() OVER (PARTITION BY c.class ORDER BY s.score DESC, s.student_id) AS rank
+--FROM scores s
+--JOIN classes c ON s.student_id = c.student_id
+--JOIN names n ON s.student_id = c.student_id 
+--)
+--SELECT class, name, score
+--FROM class_ranking
+--WHERE rank <= 2
+--ORDER BY class ASC, score DESC;
+
+--Conditional Consecutive Select:
+--Task:Display the rows which contain 3 or more consecutive days of more than 100 cases in those days.
+
+--WITH cases_over_hundred AS (
+--  SELECT * FROM cases WHERE new > 100
+--)
+--SELECT DISTINCT c1.* 
+--FROM cases_over_hundred as c1,
+--FROM cases_over_hundred as c3,
+--FROM cases_over_hundred as c2,
+--WHERE (c1.id = c2.id - 1 AND c1.id = c3.id - 2) 
+--  OR (c1.id = c2.id + 1 AND c1.id = c3.id - 1) 
+--  OR (c1.id = c2.id + 2 AND c1.id = c3.id + 1)
+--ORDER BY c1.id;
+
+--SELECT Job, Sum(Sal) TotalSum FROM EMP 
+--WHERE Job NOT LIKE ‘SALES%’
+--GROUP BY Job 
+--HAVING Sum(Sal) > 300 
+--ORDER BY Sum(Sal)
+
+--SELECT coulmn, group_function
+--FROM table
+--[WHERE condition]
+--[GROUP BY list_to_group]
+--[HAVING group_condition]
+--[ORDER BY sorting_condition]
